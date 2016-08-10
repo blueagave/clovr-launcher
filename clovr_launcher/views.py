8751da5ed2ad94efe6586a5d1b216100ca67e665
Cesar Arze <cesar.arze@gmail.com>
2016-08-10 16:58:28 +0200

    Whole bunch of changes to template files and flask code to get a prelim flask website up and running

--- clovr_launcher/views.py
+++ clovr_launcher/views.py
@@ -1,10 +1,25 @@
 # coding=utf-8
 
 from flask import render_template, flash, redirect, session, url_for, request, g, Markup
-from app import app
+from clovr_launcher import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('start_vm.html')
+
+@app.route('/start_vm')
+def start_vm():
+    return render_template('start_vm.html')
+
+@app.route('/manage_vms')
+def manage_vms():
+    return render_template('manage_vms.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('login.html')
+
+
+
 
